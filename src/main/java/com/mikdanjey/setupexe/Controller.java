e3a185d68228a2ce5336d500db26d297f51e0365
Mikdan Jey <mikdanjey@gmail.com>
2017-08-20 00:13:59 +0500

    sample

--- src/main/java/com/mikdanjey/setupexe/Controller.java
+++ src/main/java/com/mikdanjey/setupexe/Controller.java
@@ -1,4 +1,19 @@
 package com.mikdanjey.setupexe;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Label time_label;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        time_label.setText("OK");
+    }
 }
